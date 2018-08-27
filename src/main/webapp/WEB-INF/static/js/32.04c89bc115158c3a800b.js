c8f241e987030964adaa9ceee20291cab4b0d1ff
52357 <523570822@qq.com>
2018-08-27 13:24:59 +0800

    首次提交页面代码

---
+++ src/main/webapp/WEB-INF/static/js/32.04c89bc115158c3a800b.js
@@ -0,0 +1,2 @@
+webpackJsonp([32],{505:function(e,n,o){var i=o(4)(o(555),null,null,null,null);e.exports=i.exports},555:function(e,n,o){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default={name:"authredirect",created:function(){var e=window.location.search.slice(1);window.opener.location.href=window.location.origin+"/login#"+e,window.close()}}}});
+//# sourceMappingURL=32.04c89bc115158c3a800b.js.map
